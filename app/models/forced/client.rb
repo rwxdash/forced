574b7f8f8b56afb8c5f2c16c8fbd9e1789775157
Oguzhan Ozdemir <aoozdemir@live.com>
2018-11-06 01:12:20 +0300

    add annotation for models

--- app/models/forced/client.rb
+++ app/models/forced/client.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: forced_clients
+#
+#  id         :integer          not null, primary key
+#  item_type  :string
+#  item_id    :integer
+#  identifier :string
+#  deleted_at :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 module Forced
   class Client < ApplicationRecord
     belongs_to :item, polymorphic: true
--- app/models/forced/version.rb
+++ app/models/forced/version.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: forced_versions
+#
+#  id           :integer          not null, primary key
+#  client_id    :integer
+#  version      :string(255)
+#  force_update :boolean          default(FALSE)
+#  changelog    :text
+#  deleted_at   :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 module Forced
   class Version < ApplicationRecord
     belongs_to :client, class_name: 'Forced::Client'
